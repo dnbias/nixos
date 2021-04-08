8288eab45323a32f78bb60b402584eeb19e00f0d
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2021-04-08 18:27:48 +0200

    Added openssl to permitted pkgs

--- modules/desktop/apps/pgadmin.nix
+++ modules/desktop/apps/pgadmin.nix
@@ -3,21 +3,11 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.apps.pgadmin;
-
-
-{
-              nixpkgs.config.permittedInsecurePackages = [
-                "openssl-1.0.2u"
-              ];
-            };
-
-
-    
+  
 in {
   options.modules.desktop.apps.pgadmin = {
     enable = mkBoolOpt false;
   };
-  
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       pgadmin
