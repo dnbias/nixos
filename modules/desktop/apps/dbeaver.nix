9a2e5e6d74638438ffdeac3cbd1ada018ebf7080
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2021-05-17 23:22:44 +0200

    .

--- modules/desktop/apps/dbeaver.nix
+++ modules/desktop/apps/dbeaver.nix
@@ -3,14 +3,13 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.apps.dbeaver;
-  
+nixpkgs.config.permittedInsecurePackages = [
+   "libgcrypt-1.5.6"
+]; 
 in {
   options.modules.desktop.apps.dbeaver = {
     enable = mkBoolOpt false;
   };
-  nixpkgs.config.permittedInsecurePackages = [
-    "libgcrypt-1.5.6"
-  ];
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       staruml
