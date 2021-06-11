36cf60e4d22b759b7697349c80017a8cb598b4ea
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2021-06-11 21:17:53 +0200

    borders

--- modules/themes/citylights/default.nix
+++ modules/themes/citylights/default.nix
@@ -95,7 +95,8 @@ in {
         }
         (mkIf desktop.bspwm.enable {
           "bspwm/rc.d/polybar".source = ./config/polybar/run.sh;
-          "bspwm/rc.d/theme".source = ./config/bspwmrc;
+          "bspwm/rc.d/borders".source = ./config/bspwm/borders;
+          "bspwm/rc.d/theme".source = ./config/bspwm/bspwmrc;
         })
         (mkIf desktop.apps.rofi.enable {
           "rofi/theme" = { source = ./config/rofi; recursive = true; };
